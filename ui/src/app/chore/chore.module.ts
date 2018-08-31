0d74978128cb0e2b061e5ff76c2a3b0be342e2a2
onelazyugy <you@example.com>
2018-08-31 15:52:51 -0400

    styling the bill form

--- ui/src/app/chore/chore.module.ts
+++ ui/src/app/chore/chore.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ChoreComponent } from "./chore.component";
+import { ShareModule } from '../share/share.module';
 
 @NgModule({
-    declarations: [ChoreComponent],
-    imports: [CommonModule]
+    declarations: [
+        ChoreComponent
+    ],
+    imports: [
+        CommonModule,
+        ShareModule
+    ]
 })
 export class ChoreModule{}
--- ui/src/app/huni-todo/huni-todo.module.ts
+++ ui/src/app/huni-todo/huni-todo.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from "@angular/core";
 import { HuniTodoComponent } from "./huni-todo.component";
 import { CommonModule } from "@angular/common";
+import { ShareModule } from '../share/share.module';
 
 @NgModule({
-    declarations:[HuniTodoComponent],
-    imports:[CommonModule]
+    declarations:[
+        HuniTodoComponent
+    ],
+    imports:[
+        CommonModule,
+        ShareModule
+    ]
 })
 export class HuniTodoModule{}
